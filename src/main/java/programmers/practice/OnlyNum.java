8246a4ffa0f369a755092c07587609f1fcf1ee4f
kim-daeyong <kdy2353@gmail.com>
2019-07-07 18:45:53 +0900

    작성중

---
+++ src/main/java/programmers/practice/OnlyNum.java
@@ -0,0 +1,39 @@
+package programmers.practice;
+
+public class OnlyNum {
+
+
+    /*
+
+    문제 설명
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+예를 들어 s가 a234이면 False를 리턴하고 1234라면 True를 리턴하면 됩니다.
+
+제한 사항
+s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+입출력 예
+s	return
+a234	false
+1234
+     */
+
+    class Solution {
+        public boolean solution(String s) {
+            boolean answer = true;
+//            char[] list = s.toCharArray();
+            if(s.length() == 6 || s.length() == 4) {
+                for (int i = 0; i <= s.length(); i++) {
+                    char chars = s.charAt(i);
+                    if(chars >'0'|| chars<'9'){
+
+                        return answer = false;
+                    }else{
+                        return answer = true;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
