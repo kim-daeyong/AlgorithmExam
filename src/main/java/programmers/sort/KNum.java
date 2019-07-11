66da0d912ff55ab63f4effabba09a756a76bf33a
kim-daeyong <kdy2353@gmail.com>
2019-07-11 16:46:14 +0900

    K번째수

--- src/main/java/programmers/sort/KNum.java
+++ src/main/java/programmers/sort/KNum.java
@@ -1,5 +1,7 @@
 package programmers.sort;
 
+import java.util.Arrays;
+
 public class KNum {
     /*
     문제 설명
@@ -18,14 +20,126 @@ array의 각 원소는 1 이상 100 이하입니다.
 commands의 길이는 1 이상 50 이하입니다.
 commands의 각 원소는 길이가 3입니다.
 입출력 예
-array	commands	return
+array               	commands	                        return
 [1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
 
      */
 
+    //copyOfRange > array에서 from 부터 to 까지 잘라낸다.. 이걸 알기 전 까진 엄청 고생했는데 검색해서 찾아내니 엄청나게 짧아
+
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+
+        for(int i =0; i <commands.length; i++) {
+            int[] result = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(result);
+            answer[i]=result[commands[i][2]-1];
+        }
+
         return answer;
     }
 
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        KNum kNum = new KNum();
+//        kNum.solution(array, commands);
+        System.out.println(kNum.solution(array, commands));
+
+    }
+
+    /*
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int index = 0 ;
+
+        for(int i = 0 ; i < commands.length ; i++)
+        {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+
+            int[] tmp = new int[end - start + 1];
+
+            int a = 0;
+            for(int j = start - 1 ; j < end ; j++)
+                tmp[a++] = array[j];
+
+            Arrays.sort(tmp);
+            answer[index++] = tmp[k-1];
+        }
+        return answer;
+    }
+
+
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i= 0; i<commands.length ; i++){
+            int start = commands[i][0] == 0 ? 0 : commands[i][0]-1;
+            int finish = commands[i][1] == commands[i][0] ? start+1 : commands[i][1] ;
+            int pos = commands[i][2]-1;
+
+            int[] tmp = Arrays.copyOfRange(array, start, finish);
+
+            Arrays.sort(tmp);
+
+            answer[i] = tmp[pos];
+        }
+
+        return answer;
+    }
+
+
+    정렬까지..
+
+        public int[] solution(int[] array, int[][] commands) {
+        int n = 0;
+        int[] ret = new int[commands.length];
+
+        while(n < commands.length){
+            int m = commands[n][1] - commands[n][0] + 1;
+
+            if(m == 1){
+                ret[n] = array[commands[n++][0]-1];
+                continue;
+            }
+
+            int[] a = new int[m];
+            int j = 0;
+            for(int i = commands[n][0]-1; i < commands[n][1]; i++)
+                a[j++] = array[i];
+
+            sort(a, 0, m-1);
+
+            ret[n] = a[commands[n++][2]-1];
+        }
+
+        return ret;
+    }
+
+    void sort(int[] a, int left, int right){
+        int pl = left;
+        int pr = right;
+        int x = a[(pl+pr)/2];
+
+        do{
+            while(a[pl] < x) pl++;
+            while(a[pr] > x) pr--;
+            if(pl <= pr){
+                int temp = a[pl];
+                a[pl] = a[pr];
+                a[pr] = temp;
+                pl++;
+                pr--;
+            }
+        }while(pl <= pr);
+
+        if(left < pr) sort(a, left, pr);
+        if(right > pl) sort(a, pl, right);
+    }
+
+
+     */
 }
