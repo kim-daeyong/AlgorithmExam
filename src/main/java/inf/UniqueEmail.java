60ef58a3bf72904170bdfe42701b8d51ec51479c
kim-daeyong <kdy2353@gmail.com>
2020-03-12 22:59:17 +0900

    email

---
+++ src/main/java/inf/UniqueEmail.java
@@ -0,0 +1,15 @@
+package inf;
+
+public class UniqueEmail {
+    public static void main(String[] args) {
+        String[] emails = {
+
+                "testemail+wef@coding.com",
+
+                "teste.mai.l+wfwefwewef@co.ding.com",
+
+                "test.email+wefweef@coding.com"
+
+        };
+    }
+}
