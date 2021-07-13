5335f297f47cf9162fba346726361cc0f7a69aaf
kim-daeyong <kdy2353@gmail.com>
2021-07-13 15:51:40 +0900

    truck crossing the bridge

--- src/main/java/programmers/stackqueue/Truck.java
+++ src/main/java/programmers/stackqueue/Truck.java
@@ -1,5 +1,9 @@
 package programmers.stackqueue;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Truck {
 
     /*
@@ -18,10 +22,76 @@ public class Truck {
     */
     public static void main(String[] args) {
         
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
+
+        int result = solution(bridge_length, weight, truck_weights);
+
+        System.out.println(result);
+
     }
 
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
+
         int answer = 0;
-        return answer;
+        Queue<Integer> bridge = new LinkedList<>();
+        int currentWeight = 0;
+        for (int i = 0; i < truck_weights.length; i++) {
+            if (bridge.size() == bridge_length && bridge.peek() != 0) {
+                currentWeight -= bridge.poll();
+                i --;
+                continue;
+            } else {
+                if(bridge.size() > 0 && bridge.peek() == 0) {
+                    bridge.poll();
+                }
+                if (bridge.size() < bridge_length && currentWeight + truck_weights[i] <= weight) {
+                    currentWeight += truck_weights[i];
+                    bridge.offer(truck_weights[i]);
+                } else if (bridge.size() < bridge_length && currentWeight + truck_weights[i] > weight){
+                    bridge.offer(0);
+                    i --;
+                }
+            }
+            answer ++;
+        }
+        
+        return answer + bridge_length;
     }
 }
+    //     int answer = 0;
+    //     Queue<Integer> bridge = new LinkedList<>();
+    //     if (getSumWeight(truck_weights) <= weight) {
+    //         return truck_weights.length + bridge_length;
+    //     }
+    //     for (int i = 0; i < truck_weights.length; i++) {
+    //         if (bridge.size() == bridge_length && bridge.peek() != 0) {
+    //             bridge.poll();
+    //             i --;
+    //         } else {
+    //             if(bridge.size() > 0 && bridge.peek() == 0) {
+    //                 bridge.poll();
+    //             }
+    //             if (bridge.size() < bridge_length && getCurrentWeight(bridge) + truck_weights[i] <= weight) {
+    //                 bridge.add(truck_weights[i]);
+    //             } else if (bridge.size() < bridge_length && getCurrentWeight(bridge) + truck_weights[i] > weight){
+    //                 bridge.add(0);
+    //                 i --;
+    //             }
+    //         }
+    //         answer ++;
+    //     }
+        
+    //     return answer - 1;
+    // }
+
+    // public static int getCurrentWeight(Queue<Integer> bridge) {
+    //     return  bridge.stream().reduce(0, Integer::sum);
+    // }
+
+    // public static int getSumWeight(int[] truck_weights) {
+    //     return  Arrays.stream(truck_weights).reduce(0, Integer::sum);
+    // }
+
+// }
